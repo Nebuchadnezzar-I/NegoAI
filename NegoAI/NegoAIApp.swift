c4fd103b7374e00448b6187738b56f0a285ca9b8
Michal Ukropec <ukropec.michal.jr@gmail.com>
2025-04-18 11:41:50 +0200

    Initial Commit

---
+++ NegoAI/NegoAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NegoAIApp.swift
+//  NegoAI
+//
+//  Created by Michal Ukropec on 18/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct NegoAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
